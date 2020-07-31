e65b8cadd2a7c3f51baa32c72a5ad19173b8481d
Isabela Santos <isa2014mgspn@gmail.com>
2020-07-31 15:34:01 -0300

    first commit in web api project

---
+++ SmartSchool.WebAPI/Controllers/ProfessorController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfessorController :ControllerBase
+    {
+        public ProfessorController()
+        {
+            
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+          return Ok("Professor: Maria Clara, Juliana, Clarissa, Rafael, Marcos");
+        }
+    }
+}
